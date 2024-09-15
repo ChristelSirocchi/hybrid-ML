7da87e711d924e44bbd4b89049c195b2cb519320
ChristelSirocchi <75725260+ChristelSirocchi@users.noreply.github.com>
2024-09-15 17:49:29 +0200

    Add files via upload

---
+++ diabetes_update_3.pl
@@ -0,0 +1,6 @@
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+    Glucose > 126, BMI > 30.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 0) :-
+    Glucose =< 100, BMI =< 25.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+	Glucose > 121.5, BMI > 29.1.
---
+++ diabetes_update_5.pl
@@ -0,0 +1,12 @@
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+    Glucose > 126, BMI > 30.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 0) :-
+    Glucose =< 100, BMI =< 25.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+	Glucose =< 121.5, BMI > 40.75.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+	Glucose > 121.5, BMI =< 29.1, Age > 30.5.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+	Glucose > 121.5, BMI > 29.1.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, 1) :-
+	Glucose =< 121.5, BMI =< 40.75, DiabetesPedigreeFunction > 0.65, Age > 40.
